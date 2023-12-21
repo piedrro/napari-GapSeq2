5f049f6c07da38284da12df7623a46f3c359fc37
turnerp <piers.turner@protonmail.com>
2023-12-21 22:13:31 +0000

    completely reworked undrifting algo

--- src/napari_gapseq2/_widget_undrift_utils.py
+++ src/napari_gapseq2/_widget_undrift_utils.py
@@ -10,7 +10,10 @@ from functools import partial
 import concurrent.futures
 from picasso.postprocess import undrift as picasso_undrift, segment
 from picasso.imageprocess import rcc
-
+from multiprocessing import Value, Lock, cpu_count, Array, Manager
+import ctypes
+import time
+import threading
 
 
 def undrift_image(dat):
@@ -45,44 +48,104 @@ def undrift_image(dat):
 
 
 
+def detect_dataset_drift(dat, progress_dict, index):
+
+    dataset_dict = dat["dataset_dict"]
+    segmentation = dat["segmentation"]
+
+    try:
+
+        loc_dict = dataset_dict["loc_dict"]
+        undrift_locs = loc_dict["localisations"].copy()
+        picasso_info = dataset_dict["picasso_info"]
+        n_frames = picasso_info[0]["Frames"]
+
+        len_segments = n_frames // segmentation
+        n_pairs = int(len_segments * (len_segments - 1))/2
+
+        compute_progress = {"segmentation": 0,"undrift": 0}
+
+        def total_progress():
+            segmentation_progress = compute_progress["segmentation"]
+            undrift_progress = compute_progress["undrift"]
+            total_progress = int((segmentation_progress + undrift_progress)/2)
+            progress_dict[index] = total_progress
+        def segmentation_callback(progress):
+            compute_progress["segmentation"] = (progress/len_segments)*100
+            total_progress()
+
+        def undrift_callback(progress):
+            compute_progress["undrift"] = (progress/n_pairs)*100
+            total_progress()
+
+        if type(segmentation) == int:
+            if n_frames > segmentation:
+
+                drift, _ = picasso_undrift(undrift_locs,
+                    picasso_info,
+                    segmentation=segmentation,
+                    display=False,
+                    segmentation_callback=segmentation_callback,
+                    rcc_callback=undrift_callback,
+                    )
+
+                dataset_dict["drift"] = drift
+
+            else:
+                progress_dict[index] = 100
+        else:
+            progress_dict[index] = 100
+
+    except:
+        print(traceback.format_exc())
+        pass
+
+    return dataset_dict
+
+
+
+
+
+
 
 class _undrift_utils:
 
     def undrift_localisations(self):
 
         try:
-            dataset_name = self.gapseq_dataset_selector.currentText()
 
-            for channel_name, channel_data in self.dataset_dict[dataset_name].items():
-                fiducial_dict = self.localisation_dict["fiducials"][dataset_name][channel_name.lower()].copy()
+            for dataset_name, dataset_data in self.dataset_dict.items():
+                for channel_name, channel_data in self.dataset_dict[dataset_name].items():
+                    fiducial_dict = self.localisation_dict["fiducials"][dataset_name][channel_name.lower()].copy()
 
-                if "drift" in channel_data.keys() and "localisations" in fiducial_dict.keys():
-                    locs = fiducial_dict["localisations"]
+                    if "drift" in channel_data.keys() and "localisations" in fiducial_dict.keys():
+                        locs = fiducial_dict["localisations"]
 
-                    drift = channel_data["drift"]
+                        drift = channel_data["drift"]
 
-                    render_locs = {}
 
-                    for loc in locs:
-                        frame = loc.frame
-                        loc.x = loc.x - drift[loc.frame][0]
-                        loc.y = loc.y - drift[loc.frame][1]
 
-                        if frame not in render_locs.keys():
-                            render_locs[frame] = []
+                        render_locs = {}
 
-                        render_locs[frame].append([loc.y, loc.x])
+                        for loc in locs:
+                            frame = loc.frame
+                            loc.x = loc.x - drift[loc.frame][0]
+                            loc.y = loc.y - drift[loc.frame][1]
 
-                    localisation_centres = self.get_localisation_centres(locs)
+                            if frame not in render_locs.keys():
+                                render_locs[frame] = []
 
-                    self.localisation_dict["fiducials"][dataset_name][channel_name.lower()]["localisations"] = locs
-                    self.localisation_dict["fiducials"][dataset_name][channel_name.lower()]["localisation_centres"] = localisation_centres
-                    self.localisation_dict["fiducials"][dataset_name][channel_name.lower()]["render_locs"] = render_locs
+                            render_locs[frame].append([loc.y, loc.x])
+
+                        localisation_centres = self.get_localisation_centres(locs)
+
+                        self.localisation_dict["fiducials"][dataset_name][channel_name.lower()]["localisations"] = locs
+                        self.localisation_dict["fiducials"][dataset_name][channel_name.lower()]["localisation_centres"] = localisation_centres
+                        self.localisation_dict["fiducials"][dataset_name][channel_name.lower()]["render_locs"] = render_locs
 
         except:
             print(traceback.format_exc())
             pass
-
 
     def _undrift_images_finished(self):
 
@@ -100,102 +163,13 @@ class _undrift_utils:
 
         except:
             print(traceback.format_exc())
-
             self.update_ui()
 
-    def _compute_undrift(self, progress_callback, undrift_dict, segmentation=50):
+    def _apply_undrift(self, progress_callback, undrift_dict):
 
         try:
-
-            compute_progress_dict = {"segmentation": {}, "undrift": {}, "total": 0}
-
-            for dataset in undrift_dict.keys():
-                compute_progress_dict["segmentation"][dataset] = 0
-                compute_progress_dict["undrift"][dataset] = 0
-                compute_progress_dict["total"] += 200
-
-            for dataset_index, (dataset, dataset_dict) in enumerate(undrift_dict.items()):
-
-                try:
-
-                    n_locs = dataset_dict["n_locs"]
-
-                    if n_locs > 0:
-
-                        loc_dict = dataset_dict["loc_dict"]
-                        undrift_locs = loc_dict["localisations"].copy()
-                        picasso_info = dataset_dict["picasso_info"]
-
-                        n_frames = picasso_info[0]["Frames"]
-
-                        if n_frames > segmentation:
-
-                            len_segments = n_frames // segmentation
-                            n_pairs = int(len_segments * (len_segments - 1))
-
-                            def total_progress():
-                                try:
-                                    segmentation_progress = np.sum(list(compute_progress_dict["segmentation"].values()))
-                                    undrift_progress = np.sum(list(compute_progress_dict["undrift"].values()))
-                                    total = compute_progress_dict["total"]
-                                    progress = ((segmentation_progress + undrift_progress) / total) * 50
-                                    progress_callback.emit(progress)
-                                except:
-                                    print(traceback.format_exc())
-
-                            def segmentation_callback(progress, dataset=dataset):
-                                try:
-                                    compute_progress_dict["segmentation"][dataset] = (progress/len_segments)*100
-                                    total_progress()
-                                except:
-                                    pass
-
-                            def undrift_callback(progress, dataset=dataset):
-                                try:
-                                    compute_progress_dict["undrift"][dataset] = (progress/n_pairs)*100
-                                    total_progress()
-                                except:
-                                    pass
-
-                            drift, _ = picasso_undrift(undrift_locs,
-                                picasso_info, segmentation=segmentation,
-                                display=False, segmentation_callback=segmentation_callback,
-                                rcc_callback=undrift_callback, )
-
-                            undrift_dict[dataset]["drift"] = drift
-
-                        else:
-                            print(f"{dataset} is too short for undrift")
-                            compute_progress_dict["segmentation"][dataset] = 100
-                            compute_progress_dict["undrift"][dataset] = 100
-
-                    else:
-                        print(f"{dataset} has no localisations")
-                        compute_progress_dict["segmentation"][dataset] = 100
-                        compute_progress_dict["undrift"][dataset] = 100
-
-                except:
-                    print("Could not undrift dataset: {}".format(dataset))
-                    print(traceback.format_exc())
-
-        except:
-            print(traceback.format_exc())
-            undrift_dict = None
-            pass
-
-        return undrift_dict
-
-
-    def _undrift_images(self, progress_callback, undrift_dict, segmentation=20):
-
-        try:
-
-            undrift_dict = self._compute_undrift(progress_callback, undrift_dict, segmentation=segmentation)
 
             if undrift_dict != None:
-
-                dataset_list = list(undrift_dict.keys())
-                channel_list = [undrift_dict[dataset_list[0]]["channel"]]
 
                 self.shared_images = self.create_shared_images()
 
@@ -263,8 +237,68 @@ class _undrift_utils:
             print(traceback.format_exc())
             pass
 
+    def _detect_undrift(self, progress_callback, undrift_dict, segmentation=20):
 
-    def undrift_images(self):
+        try:
+
+            if undrift_dict != {}:
+                compute_jobs = []
+                progress_dict = {}
+
+                for dataset, dataset_dict in undrift_dict.items():
+
+                    compute_jobs.append({"dataset": dataset, "dataset_dict": dataset_dict, "segmentation": segmentation})
+                    progress_dict[dataset] = 0
+
+                cpu_count = int(multiprocessing.cpu_count() * 0.9)
+
+                with Manager() as manager:
+                    progress_dict = manager.dict()
+
+                    with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count) as executor:
+                        # Submit all jobs
+                        futures = [executor.submit(detect_dataset_drift, job, progress_dict, i) for i, job in enumerate(compute_jobs)]
+
+                        while any(not future.done() for future in futures):
+                            # Calculate and emit progress
+                            total_progress = sum(progress_dict.values())
+                            overall_progress = int((total_progress / len(compute_jobs))/2)
+                            if progress_callback is not None:
+                                progress_callback.emit(overall_progress)
+                            time.sleep(1)  # Update frequency
+
+                        # Wait for all futures to complete
+                        concurrent.futures.wait(futures)
+
+                        # Retrieve and process results
+                        results = [future.result() for future in futures]
+                        for result in results:
+                            if result is not None:
+                                if "drift" in result.keys():
+                                    drift = result["drift"]
+                                    dataset = result["dataset"]
+                                    undrift_dict[dataset]["drift"] = drift
+
+        except:
+            print(traceback.format_exc())
+            pass
+
+        return undrift_dict
+
+
+    def _undrift_images(self, progress_callback=None, undrift_dict={}, segmentation=20):
+
+        try:
+
+           undrift_dict = self._detect_undrift(progress_callback, undrift_dict, segmentation=segmentation)
+           self._apply_undrift(progress_callback, undrift_dict)
+
+        except:
+            print(traceback.format_exc())
+            pass
+
+
+    def undrift_images(self, segmentation=20):
 
         try:
 
@@ -286,7 +320,8 @@ class _undrift_utils:
                     picasso_info = [{'Frames': n_frames, 'Height': height, 'Width': width}, {}]
 
                     undrift_dict[dataset] = {"loc_dict": loc_dict, "n_locs": n_locs,
-                                             "picasso_info": picasso_info, "channel": channel.lower()}
+                                             "picasso_info": picasso_info,
+                                             "channel": channel.lower(), "dataset": dataset}
                 else:
                     print("No fitted localizations found for dataset: " + dataset)
 
@@ -294,15 +329,13 @@ class _undrift_utils:
 
                 self.update_ui(init=True)
 
-                self.worker = Worker(self._undrift_images, undrift_dict=undrift_dict)
+                self.worker = Worker(self._undrift_images, undrift_dict=undrift_dict, segmentation=20)
                 self.worker.signals.progress.connect(partial(self.gapseq_progress, progress_bar=self.undrift_progressbar))
                 self.worker.signals.finished.connect(self._undrift_images_finished)
-                self.worker.signals.error.connect(self.update_ui)
                 self.threadpool.start(self.worker)
 
         except:
             self.update_ui()
-
             print(traceback.format_exc())
             pass
 
